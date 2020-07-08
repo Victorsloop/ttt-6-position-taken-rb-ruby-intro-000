8a388d1e1e08edbf2fbb72f282d331306099ec81
Victor Torres <torres.victor136@gmail.com>
2020-07-08 18:11:42 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,7 @@
 def position_taken?(board, index)
   if board [index] == " " || board[index] == "" || board[index] == nil
     return false
+  else board[index] == "x" || board[index] == "O"
+    return true
   end
 end
